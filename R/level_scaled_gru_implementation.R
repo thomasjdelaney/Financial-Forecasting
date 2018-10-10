a1ccd378621dace3e9fc8085046f9d9ad0b3dc0e
Thomas James Delaney <thomas.delaney@posteo.net>
2018-10-10 16:22:11 +0100

    Level scaled network functions for non-seasonal data.
    Signed-off-by: Thomas James Delaney <thomas.delaney@posteo.net>

--- R/level_scaled_gru_implementation.R
+++ R/level_scaled_gru_implementation.R
@@ -121,6 +121,10 @@ getPredictionFromLevelParams <- function(model_predictions, level_series, alpha)
 
 preds <- predict(gru_model, x = test_samples)
 actual_preds <- array(0, dim = c(num_test_series, yearly_forecast_horizon_length))
+level_preds <- array(0, dim = c(num_test_series, yearly_forecast_horizon_length))
 for (i in 1:num_test_series){
-  actual_preds[i,] <- getPredictionFromLevelParams(preds[i,], test_levels[i,], test_alphas[[i]])[[1]]
+  descaled <- getPredictionFromLevelParams(preds[i,], test_levels[i,], test_alphas[[i]])
+  actual_preds[i,] <- descaled[[1]]
+  level_preds[i,] <- descaled[[2]]
 }
+print(mean(abs(actual_preds - test_values)))
